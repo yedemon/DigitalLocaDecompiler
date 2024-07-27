5f0ccbb37139e342ae681ae6af093fcd77754ed5
yedemon <yedemon@163.com>
2024-07-27 13:13:52 +0800

    feat: add TextureCast(BitmapCast) handler.

--- src/Digital/PCode/Reader/ScriptTextureCast.php
+++ src/Digital/PCode/Reader/ScriptTextureCast.php
@@ -7,8 +7,87 @@ namespace Digital\PCode\Reader;
 use Digital\Ast\AstFactory;
 use Digital\Ast\AstNode;
 use Digital\Ast\AstRoot;
+use Digital\PCode\PCodeReader;
 
 class ScriptTextureCast {
+    
+    const names = [
+        0x41 => 'Height',
+        0x12 => 'Name',
+        0x10 => 'Tag',
+        0x40 => 'Width',
+        0x26 => 'LoadInfo.AlphaEnabled',
+        0x30 => 'LoadInfo.ColorKey',
+        0x24 => 'LoadInfo.Foreground',
+        0x4A => 'Billboard.Alpha',
+        0x49 => 'Billboard.AlphaType',
+        0x54 => 'Billboard.ClippingValue',
+        0x56 => 'Billboard.CenterX',
+        0x57 => 'Billboard.CenterY',
+        0x4B => 'Billboard.Color.R',
+        0x4C => 'Billboard.Color.G',
+        0x4D => 'Billboard.Color.B',
+        0x52 => 'Billboard.FixSize',
+        0x48 => 'Billboard.Height',
+        0x4E => 'Billboard.Material',
+        0x58 => 'Billboard.OffsetZ',
+        0x47 => 'Billboard.Width',
+        0x53 => 'Billboard.ZBufferMode',
+        0x25 => 'LoadInfo.CreateMipmap',
+        0x2A => 'LoadInfo.Billboard.Alpha',
+        0x29 => 'LoadInfo.Billboard.AlphaType',
+        0x34 => 'LoadInfo.Billboard.ClippingValue',
+        0x36 => 'LoadInfo.Billboard.CenterX',
+        0x37 => 'LoadInfo.Billboard.CenterY',
+        0x2B => 'LoadInfo.Billboard.Color.R',
+        0x2C => 'LoadInfo.Billboard.Color.G',
+        0x2D => 'LoadInfo.Billboard.Color.B',
+        0x43 => 'LoadInfo.Billboard.FixSize',
+        0x28 => 'LoadInfo.Billboard.Height',
+        0x2E => 'LoadInfo.Billboard.Material',
+        0x38 => 'LoadInfo.Billboard.OffsetZ',
+        0x27 => 'LoadInfo.Billboard.Width',
+        0x33 => 'LoadInfo.Billboard.ZBufferMode',
+    ];
+
+    const valTypes = [
+        0x41 => PCodeReader::VAL_INT,
+        0x12 => PCodeReader::VAL_STRING,
+        0x10 => PCodeReader::VAL_INT,
+        0x40 => PCodeReader::VAL_INT,
+        0x26 => PCodeReader::VAL_BOOL,
+        0x30 => PCodeReader::VAL_INT,
+        0x24 => PCodeReader::VAL_BOOL,
+        0x4A => PCodeReader::VAL_FLOAT,
+        0x49 => PCodeReader::VAL_INT,
+        0x54 => PCodeReader::VAL_FLOAT,
+        0x56 => PCodeReader::VAL_FLOAT,
+        0x57 => PCodeReader::VAL_FLOAT,
+        0x4B => PCodeReader::VAL_FLOAT,
+        0x4C => PCodeReader::VAL_FLOAT,
+        0x4D => PCodeReader::VAL_FLOAT,
+        0x52 => PCodeReader::VAL_BOOL,
+        0x48 => PCodeReader::VAL_FLOAT,
+        0x4E => PCodeReader::VAL_BOOL,
+        0x58 => PCodeReader::VAL_FLOAT,
+        0x47 => PCodeReader::VAL_FLOAT,
+        0x53 => PCodeReader::VAL_INT,
+        0x25 => PCodeReader::VAL_BOOL,
+        0x2A => PCodeReader::VAL_FLOAT,
+        0x29 => PCodeReader::VAL_INT,
+        0x34 => PCodeReader::VAL_FLOAT,
+        0x36 => PCodeReader::VAL_FLOAT,
+        0x37 => PCodeReader::VAL_FLOAT,
+        0x2B => PCodeReader::VAL_FLOAT,
+        0x2C => PCodeReader::VAL_FLOAT,
+        0x2D => PCodeReader::VAL_FLOAT,
+        0x43 => PCodeReader::VAL_BOOL,
+        0x28 => PCodeReader::VAL_FLOAT,
+        0x2E => PCodeReader::VAL_BOOL,
+        0x38 => PCodeReader::VAL_FLOAT,
+        0x27 => PCodeReader::VAL_FLOAT,
+        0x33 => PCodeReader::VAL_INT,
+    ];
 
     // else is from script_cast.
     protected bool $script_main = false;
@@ -29,135 +108,149 @@ class ScriptTextureCast {
 
         $prop = d_u1($bytes, $offset);
 
-        if ($prop > 0x45) {
-            if ($prop > 0x51) {
-                if ($prop == 0x55) {
-
-                }
-                else {
-                    if ($prop != 0x60) {
-                        goto LABEL_117;
-                    }
-                    if (!$this->script_main) {
-LABEL_283:
-                        goto LABEL_284;
-                    }
-                    EvalSystem::digest($root, $bytes, $offset);
-                }
-                goto LABEL_305;
+        if ($prop <= 0x45) {
+            if ($prop == 0x45) {
+                $node = AstFactory::callNode($obj_idxr, 'X45', [], 0x45);
+                goto LABEL_347;
             }
-            if ($prop == 0x51) {
-                EvalSystem::digest($root, $bytes, $offset);
-                EvalSystem::digest($root, $bytes, $offset);
-                EvalSystem::digest($root, $bytes, $offset);
-                EvalSystem::digest($root, $bytes, $offset);
-
-                // BitmapCast.LoadFromTextCast
-                goto LABEL_284;
-            }
-            if ($prop == 0x46) {
-                goto LABEL_305;
-            }
-
-            if ($prop != 0x50) {
-                goto LABEL_117;
-            }
-            
-            EvalSystem::digest($root, $bytes, $offset);
-            EvalSystem::digest($root, $bytes, $offset);
-// LABEL_64:
-            goto LABEL_284;
-        }
-        if ($prop == 0x45) {
-LABEL_290:
-            goto LABEL_305;
-        }
-
-        if ($prop > 0x35) {
-            if ($prop != 0x42) {
-                if ($prop != 0x44) {
-                    goto LABEL_117;
-                }
-
-                goto LABEL_305;
-            }
-
-            goto LABEL_284;
-        }
-
-        switch ($prop) {
-            case 0x35:
-                break;
-            case 0x13:
-                break;
-            case 0x20:
-                EvalSystem::digest($root, $bytes, $offset);
-                break;
-            case 0x31:
-LABEL_284:
-                break;
-
-            default:
-LABEL_117:
-                if (!$this->script_main) {
-                    switch ($prop) {
-                        case 0x10:
-                        case 0x29:
-                        case 0x30:
-                        case 0x33:
-                            goto LABEL_290;
-                        case 0x12:
-                            goto LABEL_305;
-                        case 0x24:
-                        case 0x25:
-                        case 0x26:
-                        case 0x2E:
-                        case 0x32:
-                        case 0x39:
-                        case 0x3B:
-                        case 0x43:
-                            goto LABEL_284;
-
-                        case 0x27:
-                            goto LABEL_305;
-
-                        case 0x28:
-                            goto LABEL_305;
-
-                        case 0x2A:
-                            goto LABEL_305;
-
-                        case 0x2B:
-                            goto LABEL_305;
-
-                        case 0x2C:
-                            goto LABEL_305;
-                            
-                        case 0x2D:
-                            goto LABEL_305;
-
-                        case 0x40:
-                            goto LABEL_290;
-
-                        case 0x4E:
-                            goto LABEL_283;
-
-                        case 0x52:
-                            goto LABEL_283;
-
-                        default:
-LABEL_304:
-                            goto LABEL_305;
-                    }
-                }
-                EvalSystem::digest($root, $bytes, $offset);
+            if ($prop <= 0x35) {
                 switch ($prop) {
+                    case 0x35:
+                        // something unknown.
+                        $node = AstFactory::callNode($obj_idxr, 'X35', [], 0x35);
+                        break;
+                    case 0x13:
+                        $node = AstFactory::callNode($obj_idxr, 'X13', [], 0x13);
+                        goto LABEL_347;
+                    case 0x20:
+                        $params = [];
+                        $params[] = EvalSystem::digest($root, $bytes, $offset);
+                        $node = AstFactory::callNode($obj_idxr, 'Load', $params, 0x20);
+                        break;
+                    case 0x31:
+                        // something unknown.
+                        $node = AstFactory::callNode($obj_idxr, 'X31', [], 0x31);
+                        goto LABEL_347;
                     default:
-                        goto LABEL_304;
+                        goto LABEL_120;
                 }
-        }
-LABEL_305:
+                goto LABEL_347;
+            }
+            if ($prop == 0x42) {
+                // LoadFromTextCast
+                $params = [];
+                $params[] = EvalSystem::digest($root, $bytes, $offset);// text cast
+                $params[] = EvalSystem::digest($root, $bytes, $offset);// option
+                $params[] = EvalSystem::digest($root, $bytes, $offset);// EdgeColor
 
+                $params[] = EvalSystem::digest($root, $bytes, $offset);//MerginLeft
+                $params[] = EvalSystem::digest($root, $bytes, $offset);//MerginRight
+                $params[] = EvalSystem::digest($root, $bytes, $offset);//MerginTop
+                $params[] = EvalSystem::digest($root, $bytes, $offset);//MerginBottom
+
+                $node = AstFactory::callNode($obj_idxr, 'LoadFromTextCast', $params, 0x42);
+                goto LABEL_347;
+            }
+            if ($prop == 0x44) {
+                $node2 = EvalSystem::digest($root, $bytes, $offset); // somestring in higher version?
+
+                $propname = self::names[$prop]??'';
+                $propValType = self::valTypes[$prop]??PCodeReader::VAL_STRING;
+
+                $obj_prop = AstFactory::propNode($obj_idxr, $prop, $propname, $propValType);
+                $node = AstFactory::assignNode($obj_prop, $node2, true);
+                goto LABEL_347;
+            }
+            goto LABEL_120;
+        }
+        if ($prop <= 0x51) {
+            switch ($prop) {
+                case 0x51:
+                    // somestring in higher version?
+                    $params = [];
+                    $params[] = EvalSystem::digest($root, $bytes, $offset);
+                    $params[] = EvalSystem::digest($root, $bytes, $offset);
+                    $params[] = EvalSystem::digest($root, $bytes, $offset);
+                    $params[] = EvalSystem::digest($root, $bytes, $offset);
+                    $node = AstFactory::callNode($obj_idxr, 'X51', $params, 0x51);
+                    // if !v7?
+                    break;
+                case 0x46:
+                    $node = AstFactory::callNode($obj_idxr, 'X46', [], 0x46);
+                    goto LABEL_347;
+                case 0x50:
+                    $params = [];
+                    $params[] = EvalSystem::digest($root, $bytes, $offset);
+                    $params[] = EvalSystem::digest($root, $bytes, $offset);
+                    $node = AstFactory::callNode($obj_idxr, 'X50', $params, 0x50);
+                    break;
+                default:
+                    goto LABEL_120;
+            }
+
+            goto LABEL_347;
+        }
+        if ($prop == 0x55) {
+            $node = AstFactory::callNode($obj_idxr, 'Reset', [], 0x55);
+            goto LABEL_347;
+        }
+
+        if ( $prop != 0x60 )
+        {
+LABEL_120:
+            if ($this->script_main) {
+                // assign prop.
+                $node2 = EvalSystem::digest($root, $bytes, $offset);
+
+                $propname = self::names[$prop]??'';
+                $propValType = self::valTypes[$prop]??PCodeReader::VAL_UNKNOWN;
+
+                $obj_prop = AstFactory::propNode($obj_idxr, $prop, $propname, $propValType);
+                $node = AstFactory::assignNode($obj_prop, $node2);
+                // switch($prop) {
+                //     default:
+                //         goto LABEL_346;
+                // }
+            }
+            else {
+                $propname = self::names[$prop]??'';
+                $propValType = self::valTypes[$prop]??PCodeReader::VAL_UNKNOWN;
+
+                $obj_prop = AstFactory::propNode($obj_idxr, $prop, $propname, $propValType);
+                $node = $obj_prop;
+                // get prop.
+//                 switch($prop) {
+//                     default:
+// LABEL_346:
+//                         throw new Exception('SCRIPT RUNTIME ERROR');
+//                 }
+            }
+            goto LABEL_347;
+        }
+        // 0x60
+        {
+            if (!$this->script_main) {
+                // get prop.
+                $propname = self::names[$prop]??'';
+                $propValType = self::valTypes[$prop]??PCodeReader::VAL_UNKNOWN;
+
+                $obj_prop = AstFactory::propNode($obj_idxr, $prop, $propname, $propValType);
+                $node = $obj_prop;
+                
+                goto LABEL_347;
+            } else {
+                // assign prop.
+                $node2 = EvalSystem::digest($root, $bytes, $offset);
+
+                $propname = self::names[$prop]??'';
+                $propValType = self::valTypes[$prop]??PCodeReader::VAL_UNKNOWN;
+
+                $obj_prop = AstFactory::propNode($obj_idxr, $prop, $propname, $propValType);
+                $node = AstFactory::assignNode($obj_prop, $node2);
+            }
+        }
+
+LABEL_347:
         return $node;
     }
 
